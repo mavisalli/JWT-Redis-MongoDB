1b486905ffba7889508c81a2863fe31d94f8ed37
mavi <mavi.baris96@gmail.com>
2022-02-18 02:44:12 +0300

    Update

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,6 +7,7 @@ function verifyToken(req, res, next) {
     const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
     req.userData = decoded;
+    req.token = token;
     next();
   } catch (error) {
     return res.status(401).json({
@@ -25,7 +26,7 @@ function verifyRefreshToken(req, res, next) {
     const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
     req.userData = decoded;
 
-    //verify if token is in store or not
+    //verify if token is in redis store or not
     redis_client.get(decoded.sub.toString(), (err, data) => {
       if (err) throw err;
       if (data === null)
